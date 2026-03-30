56b3112bbf8920fcb77befa3cb213a77cfa0d7d9
Devops Kickbi <devops@kickbi.com>
2026-03-30 21:09:38 +0530

    implemented the DB

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,8 +1,11 @@
 import { FastifyPluginAsync } from "fastify";
+import User from "../models/users/user.model";
 
 const testRoutes: FastifyPluginAsync = async (app) => {
     app.get("/test", async (request, response) => {
-        return { message: "GET /test", dbuser: process.env.DB_USER };
+        const users = await User.find();
+        console.log("Users from DB:", users);
+        return { message: "GET /test", dbuser: process.env.DB_USER, users };
     });
 
     app.post("/test", async (request) => {
